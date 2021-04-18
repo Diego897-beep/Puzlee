218ca3b205b5f2a651f10cdfdbbaea02db43c35e
Diego897-beep <56550896+Diego897-beep@users.noreply.github.com>
2021-04-18 18:08:50 +0200

    No hagas ni caso carlos , cosas chungas

--- puzlee/UtilidadesSYS.cpp
+++ puzlee/UtilidadesSYS.cpp
@@ -18,7 +18,7 @@ void pausa() {
 	system("pause");
 }
 
- 
+
 uint8 colores[10] = { 0, 12, 10, 9, 14, 13, 3, 15, 8, 11 };
 // negro(0), rojo(12), verde(10), azul(9), amarillo(14), magenta(13), cian2(3), blanco(15), gris(8), cian1(11) 
 
